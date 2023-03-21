e1d573139a747dfd5f22e55c1f9b643c20208d40
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2023-03-21 15:33:51 -0500

    Update test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,4 @@
-#! user/bin/bash 
+#! /bin/bash 
 
 # This script will perform tests on default test cases.
 # Currently, it includes tpv104 and tpv1053d with smaller domains and coarse element sizes.
@@ -10,12 +10,15 @@ cd test
 # create and test case tpv104 with 4 cores
 create.newcase tpv104 tpv104
 cd tpv104 
+./case.setup 
 mpirun -np 4 eqdyna
+
 cd ..
 # need to add a script to calculate errors
 
 create.newcase tpv1053d tpv1053d
 cd tpv1053d
+./case.setup 
 mpirun -np 4 eqdyna
 cd ..
 
