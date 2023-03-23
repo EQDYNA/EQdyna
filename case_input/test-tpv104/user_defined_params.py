d74b4771c5c6a6990343ff56c8a6d1784c7e2536
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2023-03-22 22:33:56 -0500

    decimal error in python

--- case_input/test-tpv104/user_defined_params.py
+++ case_input/test-tpv104/user_defined_params.py
@@ -78,7 +78,7 @@ outputGroundMotion = 0 # output big vel GM time series for all the surface stati
 
 nucR       = 3.e3   # nucleation patch radius, m
 nucRuptVel = -9999. # nucleation rupture velocity, m/s; useful for sw and tw.
-nucdtau0   = 45.0d6 # peak shear stress increase for TPV104 and 105, Pa; useful for rsf
+nucdtau0   = 45.0e6 # peak shear stress increase for TPV104 and 105, Pa; useful for rsf
 
 #################################
 ##### Frictional variables ######
