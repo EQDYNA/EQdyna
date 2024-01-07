13d60949fa0a6d50c5ddef4653127fa210399bc3
DUNYU LIU <dunyuliu@gmail.com>
2024-01-07 16:53:06 +0800

    Refactor-depreciate contm.f90

--- src/qdct2.f90
+++ src/qdct2.f90
@@ -384,3 +384,35 @@ subroutine calcPhi4Hrgls(elemID, xl, shg)
         enddo
     enddo
 end subroutine calcPhi4Hrgls
+
+subroutine contm(shg,det,elmass,constm)
+    use globalvar
+    implicit none     
+    ! to calc lumped mass for an element
+    integer (kind=4) :: j,n,k
+    real (kind = dp) :: det,dsum,totmas,constm,temp1,temp2
+    real (kind = dp),dimension(nee) :: elmass
+    real (kind = dp),dimension(nen) :: work
+    real (kind = dp),dimension(nrowsh,nen) :: shg
+    !...initialize
+    dsum   = 0.0d0
+    totmas = 0.0d0
+    work   = 0.0d0
+    !...calculate
+    totmas = constm*w*det
+    do j=1,nen
+        temp2 = totmas*shg(nrowsh,j)**2
+        dsum = dsum + temp2
+        work(j) = work(j) + temp2
+    enddo
+    !...scale diagonal to conserve total mass
+    temp1 = totmas/dsum
+    !...store terms in a column
+    do j=1,nen
+        temp2 = temp1*work(j)
+        n = (j - 1)*ned
+        do k=1,ned
+            elmass(n + k) = temp2
+        enddo
+    enddo
+end subroutine contm
