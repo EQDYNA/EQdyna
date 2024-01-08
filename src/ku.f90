dd9037d659db065e60788d0a36ab7b4b78c82a86
DUNYU LIU <dunyuliu@gmail.com>
2024-01-08 11:07:17 +0800

    Refactor-remove useless vars

--- src/ku.f90
+++ src/ku.f90
@@ -5,16 +5,14 @@ subroutine ku
     implicit none
     include 'mpif.h'
     
-    integer (kind = 4) :: nel,m,i,j,ntemp,k,k1, non,itag,eqn,label
-    real (kind = dp) :: pstrinc,efPML(96), elresf(nee), dl(ned,nen), vl(ned,nen), al(ned,nen)
+    integer (kind = 4) :: nel, i, j, eqn
+    real (kind = dp) :: pstrinc, efPML(96), elresf(nee), al(ned,nen)
         
     time1 = MPI_WTIME()
     
     do nel = 1, numel
-        !dl(1:ned,1:nen) = d(1:ned,ien(1:nen,nel))
-        !vl(1:ned,1:nen) = v(1:ned,ien(1:nen,nel))
-        al(1:ned,1:nen) = 0.0d0
-        al(1:ned,1:nen) = al(1:ned,1:nen) + rdampm*v(1:ned,ien(1:nen,nel))
+        !al(1:ned,1:nen) = 0.0d0
+        al(1:ned,1:nen) = rdampm*v(1:ned,ien(1:nen,nel))
         al(3,1:nen)     = al(3,1:nen) + (1.0d0-C_elastic)*grav*(roumax-(gamar+1.0d0)*rhow)/roumax
 
         elresf = 0.0d0 
@@ -27,20 +25,18 @@ subroutine ku
                         x(1:3,ien(1:8,nel)))
             pstrain(nel) = pstrain(nel) + pstrinc
            
-            do i=1,nen
-                !non=ien(i,nel)
-                do j=1,ned
-                    !itag=locid(ien(i,nel))+j
-                    k=id1(locid(ien(i,nel))+j)
-                    if(k > 0) then
-                        brhs(k) = brhs(k) + elresf((i-1)*ned+j)
+            do i = 1, nen
+                do j = 1, ned
+                    eqn = id1(locid(ien(i,nel))+j)
+                    if(eqn > 0) then
+                        brhs(eqn) = brhs(eqn) + elresf((i-1)*ned+j)
                     endif
                 enddo
             enddo
         elseif (et(nel)==2) then ! PML element. 
             efPML=0.0d0 
-            do i=1,8
-                do j=1,3
+            do i = 1, 8
+                do j = 1,3
                     efPML((i-1)*12+9+j)=elresf((i-1)*3+j)
                 enddo
             enddo
@@ -49,25 +45,20 @@ subroutine ku
                         x(1:3,ien(1:8,nel)), mat(nel,1:5), eleshp(1,1,nel), &
                         eledet(nel), nel)
             do i = 1, 8
-                !non=ien(i,nel)
                 if (dof1(ien(i,nel))==12) then
-                    do j = 1, 12
-                        !itag=locid(ien(i,nel))+j
-                        eqn=id1(locid(ien(i,nel))+j)
+                    do j = 1, 12 
+                        eqn = id1(locid(ien(i,nel))+j)
                         if (eqn > 0) then
-                            brhs(eqn)=brhs(eqn)+efPML((i-1)*12+j)
+                            brhs(eqn) = brhs(eqn)+efPML((i-1)*12+j)
                         endif
                     enddo
-                elseif (dof1(ien(i,nel)) == ndof) then
-                    !itag=locid(ien(i,nel))+1
-                    eqn=id1(locid(ien(i,nel))+1)
-                    brhs(eqn)=brhs(eqn)+efPML((i-1)*12+1)+efPML((i-1)*12+2)+efPML((i-1)*12+3)+efPML((i-1)*12+10)
-                    !itag=locid(non)+2
-                    eqn=id1(locid(ien(i,nel))+2)
-                    brhs(eqn)=brhs(eqn)+efPML((i-1)*12+4)+efPML((i-1)*12+5)+efPML((i-1)*12+6)+efPML((i-1)*12+11)
-                    !itag=locid(non)+3
-                    eqn=id1(locid(ien(i,nel))+3)
-                    brhs(eqn)=brhs(eqn)+efPML((i-1)*12+7)+efPML((i-1)*12+8)+efPML((i-1)*12+9)+efPML((i-1)*12+12)				
+                elseif (dof1(ien(i,nel)) == ndof) then 
+                    eqn = id1(locid(ien(i,nel))+1)
+                    brhs(eqn) = brhs(eqn) + efPML((i-1)*12+1)+efPML((i-1)*12+2)+efPML((i-1)*12+3)+efPML((i-1)*12+10)
+                    eqn = id1(locid(ien(i,nel))+2)
+                    brhs(eqn) = brhs(eqn)+efPML((i-1)*12+4)+efPML((i-1)*12+5)+efPML((i-1)*12+6)+efPML((i-1)*12+11)
+                    eqn = id1(locid(ien(i,nel))+3)
+                    brhs(eqn) = brhs(eqn)+efPML((i-1)*12+7)+efPML((i-1)*12+8)+efPML((i-1)*12+9)+efPML((i-1)*12+12)				
                 endif
             enddo
         endif
