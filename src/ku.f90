3b904e6153470c60228357cec3d72bbf1a37a97c
DUNYU LIU <dunyuliu@gmail.com>
2024-01-08 10:49:28 +0800

    Refactor-remove useless vars

--- src/ku.f90
+++ src/ku.f90
@@ -1,12 +1,12 @@
 ! Copyright (C) 2006 Benchun Duan <bduan@tamu.edu>, Dunyu Liu <dliu@ig.utexas.edu>
+! MIT
 subroutine ku
     use globalvar
     implicit none
     include 'mpif.h'
     
     integer (kind = 4) :: nel,m,i,j,ntemp,k,k1, non,itag,eqn,label
-    real (kind = dp) :: det,constk,pstrinc,xc(3),matelement(5),esPML(21),es(12),ex(3,8),efPML(96),&
-        elresf(nee), eleffm(nee), dl(ned,nen), vl(ned,nen), al(ned,nen)
+    real (kind = dp) :: pstrinc,efPML(96), elresf(nee), dl(ned,nen), vl(ned,nen), al(ned,nen)
         
     time1 = MPI_WTIME()
     
@@ -15,35 +15,20 @@ subroutine ku
         vl(1:ned,1:nen) = v(1:ned,ien(1:nen,nel))
         al(1:ned,1:nen) = 0.0d0
         al(1:ned,1:nen) = al(1:ned,1:nen) + rdampm*vl(1:ned,1:nen)
-        !if (C_elastic==0) then
-            al(3,1:nen) = al(3,1:nen) + (1.0d0-C_elastic)*grav*(roumax-(gamar+1.0d0)*rhow)/roumax
-        !endif
-        ! do j = 1, nen
-           ! do i = 1, ned
-                ! if(i==3.and.C_elastic==0) then  !for inelastic off-fault, gravity included
-                    ! al(i,j) = al(i,j) + grav*(roumax-(gamar+1.0d0)*rhow)/roumax
-                ! endif
-            ! enddo
-        ! enddo
+        al(3,1:nen)     = al(3,1:nen) + (1.0d0-C_elastic)*grav*(roumax-(gamar+1.0d0)*rhow)/roumax
 
         elresf = 0.0d0 
-        det = eledet(nel)
-        !eleffm(1:nee)=elemass(1:nee,nel)
+        !det = eledet(nel)
         
         call contma(elemass(1:nee,nel),al,elresf)
 
         if (et(nel)==1.or.et(nel)>10) then
-            !constk = -eledet(nel)
-            !es(1:12)    = s1(ids(nel)+1:ids(nel)+12)
-            !ex(1:3,1:8) = x(1:3,ien(1:8,nel))
-            !matelement(1:5) = mat(nel,1:5)
             call qdckd(eleshp(1,1,nel),mat(nel,1:5),vl,dl, &
                         s1(ids(nel)+1:ids(nel)+12),elresf,-eledet(nel),&
                         eleporep(nel),pstrinc,x(1:3,ien(1:8,nel)))
             pstrain(nel) = pstrain(nel) + pstrinc
-            !s1(ids(nel)+1:ids(nel)+12)=es(1:12)
-            
-            do i=1,nen					
+           
+            do i=1,nen
                 non=ien(i,nel)
                 do j=1,ned
                     itag=locid(non)+j
@@ -54,24 +39,13 @@ subroutine ku
                 enddo
             enddo
         elseif (et(nel)==2) then ! PML element. 
-            efPML=0.0d0
-            esPML=0.0d0 
+            efPML=0.0d0 
             do i=1,8
                 do j=1,3
                 efPML((i-1)*12+9+j)=elresf((i-1)*3+j)
                 enddo
             enddo
-            ! do i=1,8
-                ! do j=1,3
-                    ! ex(j,i)=x(j,ien(i,nel))
-                ! enddo
-            ! enddo
-            !do i=1,21
-                !esPML(1:21)=s1(ids(nel)+1:ids(nel)+21)
-            !enddo
-            !do i=1,5 
-                !matelement(1:5)=mat(nel,1:5)
-            !enddo		
+
             call PMLwhg(vl,efPML,s1(ids(nel)+1:ids(nel)+21),x(1:3,ien(1:8,nel)),&
                         mat(nel,1:5),eleshp(1,1,nel),eledet(nel),nel)
             do i=1,8
@@ -96,11 +70,7 @@ subroutine ku
                     brhs(eqn)=brhs(eqn)+efPML((i-1)*12+7)+efPML((i-1)*12+8)+efPML((i-1)*12+9)+efPML((i-1)*12+12)				
                 endif
             enddo
-            !update the stress components 
-            !do i=1,21
-                !s1(ids(nel)+1:ids(nel)+21)=esPML(1:21)
-            !enddo
-        endif!et(nel)=1/2
-    enddo!nel loop
+        endif
+    enddo
     timeused(4) = timeused(4) + MPI_WTIME() - time1
 end subroutine ku
