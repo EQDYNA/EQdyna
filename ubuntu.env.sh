fd6d45dbdca228dcfc68c839ee9721e60cd6b9bf
DUNYU LIU <dunyuliu@gmail.com>
2024-02-02 22:03:00 +0800

    Update ubuntu.env.sh

--- ubuntu.env.sh
+++ ubuntu.env.sh
@@ -6,4 +6,4 @@
 apt-get install git vim make mpich
 apt-get install libnetcdf-dev libnetcdff-dev 
 apt-get install python3 python3-pip
-pip install numpy>=1.20 netCDF4 matplotlib xarray
+pip install numpy netCDF4 matplotlib xarray
