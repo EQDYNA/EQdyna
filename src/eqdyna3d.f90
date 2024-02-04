1f2c0d80e71f75fbc93393981a4ba9e258c2a944
DUNYU LIU <dunyuliu@gmail.com>
2024-02-04 18:34:42 +0800

    Refactor - create allocInit

--- src/eqdyna3d.f90
+++ src/eqdyna3d.f90
@@ -35,21 +35,20 @@ program EQdyna
     
     call readglobal
     call readmodelgeometry
+
     allocate(fxmin(ntotft),fxmax(ntotft),fymin(ntotft),fymax(ntotft),fzmin(ntotft),fzmax(ntotft),material(nmat,n2mat))
     allocate(nonfs(ntotft))
-    
     allocate(fltxyz(2,4,ntotft))
+
     call readfaultgeometry
     call readmaterial
-    !call readfric
     call readstations1
+
     itmp = maxval(nonfs)
-
     allocate(an4nds(2,n4nds), xonfs(2,itmp,ntotft), x4nds(3,n4nds))
-    call readstations2
-    
-    if (insertFaultType > 0) call read_fault_rough_geometry
 
+    call readstations2
+    if (insertFaultType > 0) call read_fault_rough_geometry
     call warning
     
     nplpts=0    !initialize number of time history plot
@@ -62,50 +61,8 @@ program EQdyna
     call qdcshl
 
     call mesh4num
-    
-    allocate(id1(maxm),locid(numnp),dof1(numnp),x(ndof,numnp), fnms(numnp), surface_node_id(numnp), stat=alloc_err) 
-
-    allocate(ien(nen,numel), mat(numel,5), et(numel), eleporep(numel), pstrain(numel), &
-                eledet(numel), elemass(nee,numel), eleshp(nrowsh-1,nen,numel), &
-                ss(6,numel), phi(nen,4,numel), stat=alloc_err)
-
-    eleporep = 0.0d0
-    pstrain  = 0.0d0
-    eledet   = 0.0d0
-    elemass  = 0.0d0
-
-    nftmx=maxval(nftnd) !max fault nodel num for all faults, used for arrays.
-    if(nftmx<=0) nftmx=1  !fortran arrays cannot be zero size,use 1 for 0
-    nonmx=sum(nonfs)    !max possible on-fault stations number
-    
-    allocate(nsmp(2,nftmx,ntotft), fnft(nftmx,ntotft), un(3,nftmx,ntotft),&
-                us(3,nftmx,ntotft), ud(3,nftmx,ntotft), fric(100,nftmx,ntotft),&
-                arn(nftmx,ntotft), r4nuc(nftmx,ntotft), anonfs(3,nonmx),&
-                arn4m(nftmx,ntotft), state(nftmx,ntotft), fltgm(nftmx),&
-                Tatnode(nftmx,ntotft), patnode(nftmx,ntotft))
-        
-    !INITIATION     
-    fltgm   = 0  
-    nsmp    = 0    
-    fnft    = 99999.0d0 
-    fric    = 0.0d0
-    un      = 0.0d0
-    us      = 1000.0d0
-    ud      = 0.0d0
-    arn     = 0.0d0
-    arn4m   = 0.0d0
-    r4nuc   = 0.0d0
-    anonfs  = 0
-    state   = 0.0d0
-    Tatnode = 0.0d0 
-    patnode = 0.0d0
-
-    allocate(ids(numel))
-    allocate(s1(5*maxm))
-    s1=0.0d0
-    
+    call allocInit
     call memory_estimate
-    
     call meshgen
     call checkMeshMaterial
 
@@ -123,18 +80,7 @@ program EQdyna
                 x(1,i) = x(1,i) + dx/2.0d0
            endif 
         enddo 
-     endif
-    
-    ! if (me == master) then 
-        ! write(*,*) '=                                                                   ='
-        ! write(*,*) '=                        Mesh generated                             ='
-        ! write(*,*) '=                                                                   ='
-        ! write(*,*) '= Interior model boundary                                           ='
-        ! write(*,*) '= xmax',PMLb(1),'xmin',PMLb(2),'ymax',PMLb(3),'ymin',PMLb(4),'zmin',PMLb(5)
-        ! write(*,*) '= Max element size',PMLb(6),PMLb(7),PMLb(8)
-        ! write(*,*) '= Model boundary                                           ='
-        ! write(*,*) '= xmax',xmax1,'xmin',xmin1,'ymax',ymax1,'ymin',ymin1,'zmin',zmin1        
-    ! endif
+    endif
     
     if(n4onf<=0) n4onf=1 
     allocate(fltsta(12,nplpts-1,n4onf),stat=alloc_err)
@@ -212,6 +158,50 @@ program EQdyna
     stop
     
 end program EQdyna
+
+subroutine allocInit
+    use globalvar 
+    implicit none 
+    allocate(id1(maxm),locid(numnp),dof1(numnp),x(ndof,numnp), fnms(numnp), surface_node_id(numnp)) 
+
+    allocate(ien(nen,numel), mat(numel,5), et(numel), eleporep(numel), pstrain(numel), &
+                eledet(numel), elemass(nee,numel), eleshp(nrowsh-1,nen,numel), &
+                ss(6,numel), phi(nen,4,numel))
+
+    eleporep = 0.0d0
+    pstrain  = 0.0d0
+    eledet   = 0.0d0
+    elemass  = 0.0d0
+
+    nftmx=maxval(nftnd) !max fault nodel num for all faults, used for arrays.
+    if(nftmx<=0) nftmx=1  !fortran arrays cannot be zero size,use 1 for 0
+    nonmx=sum(nonfs)    !max possible on-fault stations number
+
+    allocate(nsmp(2,nftmx,ntotft), fnft(nftmx,ntotft), un(3,nftmx,ntotft),&
+                us(3,nftmx,ntotft), ud(3,nftmx,ntotft), fric(100,nftmx,ntotft),&
+                arn(nftmx,ntotft), r4nuc(nftmx,ntotft), anonfs(3,nonmx),&
+                arn4m(nftmx,ntotft), state(nftmx,ntotft), fltgm(nftmx),&
+                Tatnode(nftmx,ntotft), patnode(nftmx,ntotft))
+
+    fltgm   = 0  
+    nsmp    = 0    
+    fnft    = 99999.0d0 
+    fric    = 0.0d0
+    un      = 0.0d0
+    us      = 1000.0d0
+    ud      = 0.0d0
+    arn     = 0.0d0
+    arn4m   = 0.0d0
+    r4nuc   = 0.0d0
+    anonfs  = 0
+    state   = 0.0d0
+    Tatnode = 0.0d0 
+    patnode = 0.0d0
+        
+    allocate(ids(numel))
+    allocate(s1(5*maxm))
+    s1      = 0.0d0
+end subroutine allocInit
 
 subroutine checkMeshMaterial
     use globalvar
